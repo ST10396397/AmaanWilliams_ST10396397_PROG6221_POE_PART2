3e3b56e40fd79d48ee40f484e8dadd56bc6150a9
Amaan Williams <129081794+ST10396397@users.noreply.github.com>
2024-05-10 14:48:02 +0200

    ingredient class created(test)

--- AmaanWilliams_ST10396397_PROG6221_POE_PART2/Ingredient.cs
+++ AmaanWilliams_ST10396397_PROG6221_POE_PART2/Ingredient.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Formats.Asn1;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,28 @@ namespace AmaanWilliams_ST10396397_PROG6221_POE_PART2
 {
     internal class Ingredient
     {
+        public string Name {  get; set; }
+        public double Quantity { get; set; }
+        public string Unit { get; set; }
+        public int Calories { get; set; }
+        public string FoodGroup { get; set; }
+
+        public Ingredient(string name,double quantity, string unit, int calories, string foodgroup)
+        {
+            Name = name;
+            Quantity = quantity;
+            Unit = unit;
+            Calories = calories;
+            FoodGroup = foodgroup;
+
+        }
+
+        public override string ToString()
+        {
+            return base.ToString()
+            {
+                return $"{Quantity} {Unit} of {Name} ({Calories} calories)";
+            }
+        }
     }
 }
